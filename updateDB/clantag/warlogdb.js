30c121477b7dc91517e0c74fb036cbf7fda3dd9c
Kamaal Farah <kamaal.f1@gmail.com>
2019-09-12 17:59:35 +0200

    Upon updating clan data, the API will return the updated data

--- updateDB/clantag/warlogdb.js
+++ updateDB/clantag/warlogdb.js
@@ -1,15 +1,33 @@
 const ClanWarlog = require('../../schemas/clantag/clanWarlog_schema');
 
-module.exports = (clan, parsed) => {
+module.exports = async (clan, parsed) => {
+  try {
+    const data = parsed.items.reduce(
+      (acc, logs) => {
+        const { clanScore, trophyChange, createdDate } = acc;
+        const [log] = logs;
+
+        const clansStanding = log.standings.find(standing => standing.clan.tag === `#${clan}`);
+
+        return {
+          createdDate: [...createdDate, log.createdDate],
+          clanScore: [...clanScore, clansStanding.clan.clanScore],
+          trophyChange: [...trophyChange, clansStanding.trophyChange],
+        };
+      },
+      { clanScore: [], trophyChange: [], createdDate: [] },
+    );
+
     const condition = { id: clan };
-    const update = { id: clan, items: parsed.items };
+    const update = { id: clan, clanWarlog: { logs: parsed.items, data } };
     const options = { upsert: true };
 
-    ClanWarlog.findOneAndUpdate(condition, update, options, error => {
-        if (error) {
-            return console.error(`Save Failed(warlog) ${clan}`, error);
-        }
+    const entity = await ClanWarlog.findOneAndUpdate(condition, update, options);
 
-        console.log(`Saved warlog ${clan}`);
-    });
+    console.log(`Saved warlog ${clan}`);
+
+    return { clanWarlog: entity.toJSON() };
+  } catch (error) {
+    return console.error(`Save Failed(warlog) ${clan}`, error);
+  }
 };
--- updateDB/playertag/battlelogdb.js
+++ updateDB/playertag/battlelogdb.js
@@ -4,28 +4,28 @@ module.exports = async (player, parsed) => {
   try {
     const data = parsed.reduce(
       (acc, log) => {
-        const {startingTrophies, trophyChange, battleTime} = acc;
+        const { startingTrophies, trophyChange, battleTime } = acc;
 
-        if (log.team[0].trophyChange !== undefined) {
-          startingTrophies.push(log.team[0].startingTrophies);
-          trophyChange.push(log.team[0].trophyChange);
-          battleTime.push(log.battleTime);
-        }
+        if (log.team[0].trophyChange === undefined) return acc;
 
-        return acc;
+        return {
+          startingTrophies: [...startingTrophies, log.team[0].startingTrophies],
+          trophyChange: [...trophyChange, log.team[0].trophyChange],
+          battleTime: [...battleTime, log.battleTime],
+        };
       },
-      {startingTrophies: [], trophyChange: [], battleTime: []},
+      { startingTrophies: [], trophyChange: [], battleTime: [] },
     );
 
-    const condition = {id: player};
-    const update = {id: player, battlelog: {logs: parsed, data}};
-    const options = {upsert: true};
+    const condition = { id: player };
+    const update = { id: player, battlelog: { logs: parsed, data } };
+    const options = { upsert: true };
 
     const entity = await Battlelog.findOneAndUpdate(condition, update, options);
 
     console.log(`Saved battlelog ${player}`);
 
-    return {battlelog: entity.toJSON()};
+    return { battlelog: entity.toJSON() };
   } catch (error) {
     return console.error(`Save Failed(battlelog) ${player}`, error);
   }
